9d64f26d3620282a9bb4a11e988e71161b781587
Katie Cutrone <kecutrone@gmail.com>
2020-09-15 21:07:56 -0600

    song push

--- scripts/song-list.js
+++ scripts/song-list.js
@@ -4,17 +4,17 @@
       <tr>
       <td>
       <button>
-      <span class="song-number">${index + 1}</span>
-       <span class="ion-play"></span>
-       <span class="ion-pause"></span>
-       </button>
-       </td>
+        <span class="song-number">${index + 1}</span>
+        <span class="ion-play"></span>
+        <span class="ion-pause"></span>
+      </button>
+    </td>
        <td>${song.title}</td>
        <td>${song.duration}</td>
        </tr>
      `);
 
-    song.element.on('click'. event => {
+    song.element.on('click', event => {
       player.playPause(song);
     });
 
